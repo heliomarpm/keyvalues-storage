4805ebe74aca99238e0653bb8bf3d339a7933b22
Heliomar P. Marques <heliomarpm@gmail.com>
2023-04-16 12:57:13 -0300

    teste path

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,19 @@
 import { KeyValues } from "./keyvalues";
 
+import { join, resolve } from 'node:path';
+
+export function DirName(): string {
+    return join(__dirname, 'db');
+}
+
+export function Resolve(): string {
+    return resolve('.', 'db');
+}
+
+
+export function Resolve2(): string {
+    return resolve('db');
+}
+
+
 export { KeyValues }
