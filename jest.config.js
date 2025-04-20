e8232987ef1c5ed7ccbbe71d9718b4d58a63d751
Heliomar P. Marques <heliomarpm@gmail.com>
2025-04-20 20:39:00 -0300

    chore: update configuration files for ESLint, Prettier, and TypeScript; adjust .editorconfig and .gitignore

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,11 @@
 module.exports = {
-    preset: 'ts-jest',
-    testEnvironment: 'node',
-    coveragePathIgnorePatterns: [
-        "/node_modules/",
-        "dist"
-    ]
+	preset: 'ts-jest',
+	testEnvironment: 'node',
+	coveragePathIgnorePatterns: [
+		"/node_modules/",
+		"dist"
+	],
+	"moduleNameMapper": {
+		"@/(.*)$": "<rootDir>/src/$1"
+	}
 };
