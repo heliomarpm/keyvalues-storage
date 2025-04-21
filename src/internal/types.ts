e6a02ac357aa5cc00c4d1e4c1f13a2dae6c777bf
Heliomar P. Marques <heliomarpm@gmail.com>
2025-04-20 21:25:37 -0300

    chore: clean up type definitions in types.ts for improved readability

--- src/internal/types.ts
+++ src/internal/types.ts
@@ -1,4 +1,3 @@
-
 /**
  * `Options` types contain all the configuration options for
  * Options that can be set in the constructor of KeyValues Class
@@ -6,47 +5,43 @@
  *  new KeyValues({fileName: 'config.json'})
  */
 type Options = {
-  /**
-   * Whether or not to save the settings file atomically.
-   */
-  atomicSave: boolean;
+	/**
+	 * Whether or not to save the settings file atomically.
+	 */
+	atomicSave: boolean;
 
-  /**
-   * The path to the settings directory. Defaults to your
-   * app's user data direcory.
-   */
-  dir?: string;
+	/**
+	 * The path to the settings directory. Defaults to your
+	 * app's user data direcory.
+	 */
+	dir?: string;
 
-  // /**
-  //  * A custom Electron instance to use. Great for testing!
-  //  */
-  // electron?: typeof Electron;
+	// /**
+	//  * A custom Electron instance to use. Great for testing!
+	//  */
+	// electron?: typeof Electron;
 
-  /**
-   * The name of the settings file that will be saved to
-   * the disk.
-   */
-  fileName: string;
+	/**
+	 * The name of the settings file that will be saved to
+	 * the disk.
+	 */
+	fileName: string;
 
-  /**
-   * Whether or not to prettify the data when it's saved to
-   * disk.
-   */
-  prettify: boolean;
+	/**
+	 * Whether or not to prettify the data when it's saved to
+	 * disk.
+	 */
+	prettify: boolean;
 
-  // /**
-  //  * The number of spaces to use when stringifying the data
-  //  * before saving to disk if `prettify` is set to `true`.
-  //  */
-  numSpaces: number;
+	// /**
+	//  * The number of spaces to use when stringifying the data
+	//  * before saving to disk if `prettify` is set to `true`.
+	//  */
+	numSpaces: number;
 };
 
-
-
-type KeyPath = string | Array<string | number>;
-type Types<T extends valueTypes> = Record<string, T>
+type KeyPath = string | Array<string>;
 
 type valueTypes = null | string | number | boolean | object | dictionaryType | valueTypes[];
-type dictionaryType = {
-  [key: string]: valueTypes;
-}
+type Types<T extends valueTypes> = Record<string, T>;
+type dictionaryType = { [key: string]: valueTypes };
