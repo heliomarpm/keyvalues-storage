1cb7b9ad833a2039ffba727e6974ad4b3c40990a
Heliomar P. Marques <heliomarpm@gmail.com>
2025-06-22 00:17:38 -0300

    chore: 🗑️ remover arquivo de configuração do Jest
    - O arquivo jest.config.js foi removido pois não é mais necessário
    - Simplifica a estrutura do projeto ao eliminar configurações não utilizadas

--- jest.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-	preset: 'ts-jest',
-	testEnvironment: 'node',
-	coveragePathIgnorePatterns: ['/node_modules/', 'dist'],
-	moduleNameMapper: {
-		'@/(.*)$': '<rootDir>/src/$1'
-	}
-};
